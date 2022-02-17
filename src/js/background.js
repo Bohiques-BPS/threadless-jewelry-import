c0d5ab13aeef0dc8309721acbc210a9c377b331f
moises <elmoises.reydereymail.com>
2022-02-17 11:32:26 -0400

    solucionado el error de guardado de productos

--- src/js/background.js
+++ src/js/background.js
@@ -3,19 +3,21 @@ chrome.runtime.onInstalled.addListener( function(){
 });
 
 chrome.runtime.onConnect.addListener( function(port) {
-    console.log( "Connected" )
+    console.log( "Connected,moi" )
     console.log(port)
     port.onMessage.addListener( function(msg,port){
+        console.log(msg)
         // let name = port.name, 
         //     url = port.sender.url,
         //     tabId = port.sender.tab.id;
         chrome.storage.local.get("products", function(data){
             let products = [];
+            console.log(data)
             if( Object.keys(data).length !== 0 ) {
-                products = data;
+                products = data.products;
             }
             products.push( msg.product );
-            chrome.storage.local.set("products", products);
+            chrome.storage.local.set({products: products});
         });
 
     })
