1a173aaa02090057318bfb49942ba760657e6c2c
Octavio Martinez <omartinez@logoscorp.com>
2022-02-21 17:07:40 -0400

    download

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,48 +1,47 @@
-$(document).ready( async _ => {
-    let { products } = await chrome.storage.local.get("products")
+$(document).ready(async (_) => {
+  let { products } = await chrome.storage.local.get("products");
 
-    $("#clear").click( _ => {
-        chrome.storage.local.set({products:[]});
-        products = [];
-    })
+  $("#clear").click((_) => {
+    chrome.storage.local.set({ products: [] });
+    products = [];
+  });
 
-    $("#download").click( _ => {
-        
-        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(getContent(products));
-        // var dlAnchorElem = document.getElementById('download2');
-        // dlAnchorElem.setAttribute("href",dataStr);
-        // dlAnchorElem.click();
-        
-    })
+  $("#download").click((_) => {
+    var dataStr =
+      "data:text/json;charset=utf-8," +
+      encodeURIComponent(getContent(products));
+    var dlAnchorElem = document.getElementById("download2");
+    dlAnchorElem.setAttribute("href", dataStr);
+    dlAnchorElem.click();
+  });
 
-    function getContent(products){
-        let str = "post_title,post_content,sku,regular_price,sale_price\n"
-        console.log(products)
-        products.forEach( product => {
-            product.variations.forEach( variation => {
-                let price = 
-                str += `${product.title},${product.description},${variation.code},${variation.price},${variation.greaterThan3}\n`;
+  function getContent(products) {
+    let str = "post_title,post_content,sku,regular_price,sale_price\n";
+    console.log(products);
+    products.forEach((product) => {
+      product.variations.forEach((variation) => {
+        let price =
+          (str += `${product.title},${product.description},${variation.code},${variation.price},${variation.greaterThan3}\n`);
+      });
+    });
+    console.log(str);
+    return str;
+  }
 
-            });
-        })
-        console.log(str)
-        return str;
-    }
+  setInterval((_) => {
+    let html = "<table>";
+    products.forEach((product) => {
+      html += "<tr style='border: 1px solid grey; padding:5px;'>";
+      html += "<td>" + product.title + "</td>";
+      html += "<td>" + product.itemNumber + "</td>";
+      html += "</tr>";
+    });
+    html += "</table>";
+    console.log(html);
+    $("#productos").html(html);
+  }, 200);
 
-    setInterval(_=>{
-        let html = "<table>";
-        products.forEach( product => {
-            html += "<tr style='border: 1px solid grey; padding:5px;'>"
-            html += "<td>"+product.title+"</td>"
-            html += "<td>"+product.itemNumber+"</td>"
-            html += "</tr>"
-        })
-        html += "</table>";
-        console.log(html)
-        $("#productos").html(html)
-    },200)
-
-    /*
+  /*
     description: "\n\t\t\t\t\t\t\tWe use Ti-6AL4V-ELi ASTM F-136(Grade 23) *IMPLANT GRADE* titanium in all of our Titanium Body Jewelry.\t\t\t\t\t\t\tView G23 Titanium Certificate\t\t\t\t\t\t"
     itemNumber: "T23RX1"
     material: "TITANIUM 6AL-4V-ELI ASTM F-136"
@@ -75,5 +74,4 @@ plating: "PVD"
 soldBy: "Crystals"
 title: "316L Surgical Steel Hinged Segment Hoop Rings with 5 Flush Set Front Facing Petite Crystals"
 variations: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]*/
-} );
-
+});
